5048c4664bb20cc7af85dbc283fe154ce63c4bb9
rtong123 <74886824+rtong123@users.noreply.github.com>
2020-11-23 11:02:57 +0700

    Promise Function

--- src/index.js
+++ src/index.js
@@ -6,15 +6,18 @@
 //simulateAsyncAPI("B", 500);
 //simulateAsyncAPI("C", 100);
 
-function simulateAsyncAPI(text, time, callback) {
-  setTimeout(() => {
-    console.log(text);
-    callback();
-  }, time);
-}
-
-simulateAsyncAPI("A", 1000, () => {
-  simulateAsyncAPI("B", 500, () => {
-    simulateAsyncAPI("C", 100, () => {});
+function simulateAsyncAPI(text, time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(text);
+      resolve();
+    }, time);
   });
-});
+}
+simulateAsyncAPI("A", 1000)
+  .then(() => {
+    return simulateAsyncAPI("B", 500);
+  })
+  .then(() => {
+    return simulateAsyncAPI("C", 100);
+  });
