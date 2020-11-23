7d49f643157a0b0e596549cbafa9962bc7f87cca
rtong123 <74886824+rtong123@users.noreply.github.com>
2020-11-23 10:31:53 +0700

    calculator js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-let nickname = "rtong";
-console.log(nickname);
+import * as c from "./calculation";
+console.log(c.add(2, 2));
+console.log(c.multiply(2, 6));
+
+// import * as cal from "./calculation";
+// console.log(cal.add(2, 2));
+// console.log(cal.multiply(2, 3));
